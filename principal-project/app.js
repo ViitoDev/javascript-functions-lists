0a26fc6e2773701a71ef0ec94fb6dffb6b2119bf
João Barros <joaovictorpiresdev@gmail.com>
2026-05-15 07:30:33 -0300

    feat: render heading and paragraph text from JS

---
+++ principal-project/app.js
@@ -0,0 +1,5 @@
+let title = document.querySelector('h1');
+title.textContent = 'Secret number game';
+
+let paragraph = document.querySelector('p');
+paragraph.textContent = 'Choose a number: 0 - 10'
