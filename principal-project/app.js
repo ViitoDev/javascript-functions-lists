eb54d6148a056fe56a2d9675f22332234eb7220c
João Barros <joaovictorpiresdev@gmail.com>
2026-05-15 08:07:12 -0300

    feat: View the button has clicked

--- principal-project/app.js
+++ principal-project/app.js
@@ -2,4 +2,8 @@ let title = document.querySelector('h1');
 title.textContent = 'Secret number game';
 
 let paragraph = document.querySelector('p');
-paragraph.textContent = 'Choose a number: 0 - 10'
+paragraph.textContent = 'Choose a number: 0 - 10';
+
+function verifyGuess() {
+    console.log('The button has clicked')
+};
